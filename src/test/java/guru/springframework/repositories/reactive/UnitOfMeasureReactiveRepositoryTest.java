53c096920610ca9327a97515c689e34df7e4d0e9
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-04-15 17:28:18 +0200

    Tests for reactive repositories added

---
+++ src/test/java/guru/springframework/repositories/reactive/UnitOfMeasureReactiveRepositoryTest.java
@@ -0,0 +1,51 @@
+package guru.springframework.repositories.reactive;
+
+import guru.springframework.domain.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataMongoTest
+public class UnitOfMeasureReactiveRepositoryTest {
+
+    private static final String EACH = "EACH";
+
+    @Autowired
+    UnitOfMeasureReactiveRepository unitOfMeasureReactiveRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        unitOfMeasureReactiveRepository.deleteAll().block();
+    }
+
+    @Test
+    public void testSaveUom(){
+        UnitOfMeasure each = new UnitOfMeasure();
+        each.setDescription(EACH);
+
+        unitOfMeasureReactiveRepository.save(each).block();
+
+        Long count = unitOfMeasureReactiveRepository.count().block();
+
+        assertEquals(Long.valueOf(1), count);
+    }
+
+    @Test
+    public void testFindByDescription() throws Exception {
+        UnitOfMeasure uom = new UnitOfMeasure();
+        uom.setDescription(EACH);
+
+        unitOfMeasureReactiveRepository.save(uom).block();
+
+        UnitOfMeasure fetchedUOM = unitOfMeasureReactiveRepository.findByDescription(EACH).block();
+
+        assertEquals(EACH, fetchedUOM.getDescription());
+
+    }
+}
